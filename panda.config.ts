314565a04cb3055c3240048df28a6e544efbb780
Aarón Rafael <69669283+Chewawi@users.noreply.github.com>
2024-12-31 17:57:12 -0300

    fix: forgor 2

--- panda.config.ts
+++ panda.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "@pandacss/dev";
-import { fadeInFromTopAndOutTop } from "~/app/page";
+
+export const fadeInFromTopAndOutTop = {
+	initial: { y: -5, x: 0, opacity: 0 },
+	enter: { y: 0, opacity: 1 },
+	exit: {
+		y: -5,
+		opacity: 0,
+		transition: { duration: 0.2 },
+	},
+};
 
 export default defineConfig({
 	preflight: true,
