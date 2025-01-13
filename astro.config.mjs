3c182f7624d495b1c82e656a95461ae50d4e9d11
Aarón Rafael <69669283+Chewawi@users.noreply.github.com>
2025-01-13 02:41:18 -0300

    feat: add Vercel adapter and set output to "server"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
 	experimental: {
 		svg: true,
 	},
+
+	adapter: vercel(),
+	output: "server"
 });
